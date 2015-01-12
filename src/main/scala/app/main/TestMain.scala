3fe032333603531669f9e708a50bc49aef14a350
Toni Pesola <pesola.toni@gmail.com>
2015-01-12 18:11:23 +0200

    Parsing test

--- src/main/scala/app/main/TestMain.scala
+++ src/main/scala/app/main/TestMain.scala
@@ -6,13 +6,33 @@ import tilde.graphics._
 import tilde.entity.component._
 import scala.collection.mutable.ListBuffer
 import tilde.util._
+import scala.util.parsing.json._
 /**
  * Created by Toni on 26.12.14.
  */
+class MyType(val name: String, val tpe: String, val pos: Other)
+
+
+class Other(val x: Float,val y: Float)
+
+
+
 object TestMain {
+  implicit def OtherCodec: CodecJson[Other] = codec2(
+    (x: Float, y: Float) => new Other(x, y),
+    (other: Other) => (other.x, other.y)
+    )("x", "y")
+
+  implicit def MyTypeCodec: CodecJson[MyType] = codec3(
+    (name: String, tpe: String, pos: Other) => new MyType(name, tpe, pos),
+    (myType: MyType) => (myType.name, myType.tpe, myType.pos)
+    )("name", "type", "pos")
+
   def main(args: Array[String]): Unit = {
     val map = ResourceUtil.readFromFile("src/main/resources/"
       + "maps/untitled.map")
-println(map)
-}
+    println(map)
+    val res = JSON.parseFull(map)
+    println(res)
+  }
 }
